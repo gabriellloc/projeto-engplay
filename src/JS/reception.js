487859692dbca398a44357b91d219a53434d6685
gabriellloc <gabrielocdev595@gmail.com>
2025-09-03 23:44:26 -0300

    aplicando e personalizando o modal de reception

--- src/JS/reception.js
+++ src/JS/reception.js
@@ -1,8 +1,25 @@
 const body = document.querySelector("body")
+let userName = "Isadora"
 
 export function reception(){
   const receptionModal = document.createElement("dialog")
+  receptionModal.classList.add("receptionModal")
+
+  const receptionDiv = document.createElement("div")
+  receptionDiv.classList.add("receptionDiv")
+
+  receptionDiv.innerHTML = `
+    <img src="assets/icons/logo (ENGPLAY).svg" alt="Logo" class="logo">
+    <img src="assets/icons/coracao.svg" alt="Coração" class="heart">
+    <h1 class="byeTitle">Sentiremos sua falta, <span class="capitalize">${userName}</span>!!</h1>
+    <p class="byeText">Respeitamos totalmente sua decisão de encerar sua assinatura. Porém caso haja algo que possamos fazer para melhorar sua experiência, queremos ouvi-lo antes de nos despedirmos.</p>
+    <div>
+        <button class="manterAss keepSub">Manter Assinatura</button>
+        <button class="continueBnt" id="bntFeedback">Continuar</button>
+    </div>
+  `
   
+  receptionModal.append(receptionDiv)
   body.appendChild(receptionModal)
   receptionModal.showModal()
 }
