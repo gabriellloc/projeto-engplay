01f33674b98cc02448b7c1c4217497cc81a41b74
gabriellloc <gabrielocdev595@gmail.com>
2025-09-04 23:35:43 -0300

    progress bar and feedback initial

--- src/JS/feedback.js
+++ src/JS/feedback.js
@@ -1,6 +1,31 @@
 export function feedback(){
   const mainContent = document.querySelector("#mainContent")
-  mainContent.innerHTML = `
-    
+  mainContent.innerHTML = ""
+  const progressBarHTML = document.createElement("div")
+  progressBarHTML.innerHTML = `
+    <section class="progress-bar">
+      <div class="bar-stage">
+        <div class="number-bar active-background">1</div>
+        <label class="text-bar active-color">FEEDBACK</label>
+      </div>
+
+      <div class="bar-stage center-bar">
+        <div class="number-bar">2</div>
+        <label class="text-bar">BENEFÍCIOS</label>
+      </div>
+
+      <div class="bar-stage">
+        <div class="number-bar">3</div>
+        <label class="text-bar">FINALIZAÇÃO</label>
+      </div>
+    </section>
   `
+
+  const feedbackForm = document.createElement("form")
+  feedbackForm.innerHTML = `
+    <h1>Podemos ouvir você rapidinho ?</h1>
+    <hr>
+    <p>Entender seu motivo nos ajuda a evoluir. É rápido, basta escolher uma opção abaixo</p>
+  `
+  mainContent.append(progressBarHTML, feedbackForm)
 }
