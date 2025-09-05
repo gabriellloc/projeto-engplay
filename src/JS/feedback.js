ce6750e8e935e614a7620c80b92fcc3d74de445f
gabriellloc <gabrielocdev595@gmail.com>
2025-09-05 00:22:31 -0300

    colocando o conteudo do form e o botao de keepSub

--- src/JS/feedback.js
+++ src/JS/feedback.js
@@ -1,3 +1,5 @@
+import { keepSubs } from "./bntKeepSub"
+
 export function feedback(){
   const mainContent = document.querySelector("#mainContent")
   mainContent.innerHTML = ""
@@ -26,6 +28,44 @@ export function feedback(){
     <h1>Podemos ouvir você rapidinho ?</h1>
     <hr>
     <p>Entender seu motivo nos ajuda a evoluir. É rápido, basta escolher uma opção abaixo</p>
+    <div>
+      <input type="radio" name="feedback" id="dificuldades">
+      <label for="dificuldades">Dificuldades com o suporte ao cliente.</label>
+    </div>
+
+    <div>
+      <input type="radio" name="feedback" id="noTime" value="noTime">
+      <label for="noTime">Falta de tempo.</label>
+    </div>
+
+    <div>
+      <input type="radio" name="feedback" id="financial" value="financial">
+      <label for="financial">Questões financeira.</label>
+    </div>
+
+    <div>
+      <input type="radio" name="feedback" id="needs" value="needs">
+      <label for="needs">Conteúdo não atendeu às minhas necessidades.</label>
+    </div>
+
+    <div>
+      <input type="radio" name="feedback" id="objective">
+      <label for="objective">Já atingi o meu objetivo.</label>
+    </div>
+
+    <div>
+      <label for="Other">Outro motivo:</label>
+      <input type="text" id="Other" value="">
+    </div>
+
+    <div class="divBnt">
+      <button class="manterAss keepSub">Manter Assinatura</button>
+      <button class="continueBnt" id="">Continuar</button>
+    </div>
   `
+  feedbackForm.addEventListener("submit", (event)=> {
+    event.preventDefault()
+  })
   mainContent.append(progressBarHTML, feedbackForm)
+  keepSubs()
 }
